79541e5a72cf0ac117184747fefca3c35175772a
gitdagray <DaveOnEleven@gmail.com>
2021-10-14 22:50:42 -0500

    first commit

---
+++ controllers/refreshTokenController.js
@@ -0,0 +1,37 @@
+const usersDB = {
+    users: require('../model/users.json'),
+    setUsers: function (data) { this.users = data }
+}
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const handleRefreshToken = (req, res) => {
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.sendStatus(401);
+    const refreshToken = cookies.jwt;
+
+    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
+    if (!foundUser) return res.sendStatus(403); //Forbidden 
+    // evaluate jwt 
+    jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        (err, decoded) => {
+            if (err || foundUser.username !== decoded.username) return res.sendStatus(403);
+            const roles = Object.values(foundUser.roles);
+            const accessToken = jwt.sign(
+                {
+                    "UserInfo": {
+                        "username": decoded.username,
+                        "roles": roles
+                    }
+                },
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: '30s' }
+            );
+            res.json({ accessToken })
+        }
+    );
+}
+
+module.exports = { handleRefreshToken }
